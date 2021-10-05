d49fe6871f879407a90481edb3118760cba684dc
lukemunsch <lpmunsch@gmail.com>
2021-10-05 20:39:40 +0000

    add code for initial user input and validation check

--- run.py
+++ run.py
@@ -1,15 +1,47 @@
 import gspread
 from google.oauth2.service_account import Credentials
-from pprint import pprint
 
 SCOPE = [
-    "https://www.googleapis.com/auth/spreadsheets"
-    "https://www.googleapis.com/auth/drive.file"
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
-]
+    ]
 
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('promo-sales-review')
+
+def action_request():
+    """
+    This will set up the functions for specific requests by the admins
+    The user will need to specifically state their intention
+    """
+    print("For the daily sales review, please choose which action you would like to run.")
+    print("Your input must be exactly as the choices are given below.\n")
+    print("Please choose from the following options: sales, stock, advisor.\n")
+
+    choice = input("Enter your choice of action here: \n")
+
+    validate_choice(choice)
+
+def validate_choice(value):
+    """
+    This is used to make sure the input from user is the correct value
+    by making the format and type of input matches exact request.
+    """
+    try:
+        if value != 'sales' or 'stock' or 'advisor':
+            raise ValueError(
+                f"You must choose one of the options provided,\nyou chose {value}"
+            )
+    except ValueError as e:
+        print(f"Invalid selection: {e}, please try again.\n")
+    else:
+        print(f"You have chosen {value.capitalize()}\n")
+
+
+
+print('Welcome to the Promotional Sales Review System!\n')
+action_request()
 
