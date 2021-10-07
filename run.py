a621ea6ce835fc5f117af8993197e2f6f8e1c468
lukemunsch <lpmunsch@gmail.com>
2021-10-07 15:34:08 +0000

    add code to create a list of devices and the full list of items sold.

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -11,6 +12,7 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('promo-sales-review')
+
 
 def password_request():
     """
@@ -27,7 +29,6 @@ def password_request():
         if validate_choice(password):
             print("Loading systems...")
             break
-            
         
 
 def validate_choice(value):
@@ -45,29 +46,34 @@ def validate_choice(value):
         print(f"You are authorised.\n")
         return True
 
-
-def get_sales_data():
+def build_item_list():
     """
-    This function will find all the relevant sales data. This will allow
-    us to build three dictionaries for:
-    - average sales for each device
-    - device with most sales, and
-    - total number of news and upgrades sold.
+    this will build a list of devices to use in the other functions
     """
-    print(f"Activating Sales Data Retrieval...\n")
-    sales = SHEET.worksheet('sales')
-    device_list = []
+    devices = SHEET.worksheet('items').row_values(1)
+    return devices
 
-def get_stock_data():
-    stock = SHEET.worksheet('stock')
-    stock_row = stock.get_values[-1]
-    print(stock_row)
+def create_items_sold_list():
+    """
+    This is the function to count how many of each device has been
+    sold since the start of the promotional sales period.
+    """
+    print("We are now building the item sales counter")
+    sales_count = SHEET.worksheet('sales')
+    count_list = sales_count.col_values(4)
+    del count_list[0]
+    return count_list
+
+def
 
 def main():
     """
     Runn all programme functions
     """
-    password_request()
+    # password_request()
+    item_list = build_item_list()
+    countable = create_items_sold_list()
+
 
 print('Welcome to the Promotional Sales Review System!\n')
 main()
