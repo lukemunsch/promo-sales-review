f3986b5145952bede59176fe49a9d6a36b791f24
lukemunsch <lpmunsch@gmail.com>
2021-10-08 20:10:31 +0000

    add code to calculate the most sold device and it value to be displayed

--- run.py
+++ run.py
@@ -83,7 +83,7 @@ def count_sales_value(value1, value2):
     This will calculate the total value of the sales made so far
     as well as the average sales value.
     """
-    print("We are finding the TOTAL and AVERAGE sales values...\n")
+    print("Calculating the TOTAL and AVERAGE sales values...\n")
     total_val = sum(value1)
     avg_val = sum(value1) / value2
     print("We have finished calculations.\n")
@@ -102,11 +102,46 @@ def create_unique_value_list(value):
     return new_list
 
 
+def print_list(value1, value2):
+    """
+    This will print a list of the advisors who have sold something,
+    then print a list of devices that have been sold.
+    """
+    print("Here are the devices that have been sold during the promotion...\n")
+    print(value2)
+    print("")
+    print("Here are the advisors that have participated in the sales promotion...\n")
+    print(value1)
+    print("")
+
+
+
+def create_dict_count(value):
+    """
+    this will count the number of occurances of all items in a list
+    """
+    counted = dict([(i, value.count(i)) for i in value])
+
+    for key, value in counted.items():
+        print(key, ' : ', value)
+    print("")
+    return counted
+
+
+def find_max_key_val(value):
+    most_sold = max(value, key=value.get)
+    keys_val = value.get(most_sold)
+    print(f"We can see the most sold item is {most_sold},")
+    print(f"with a total of {keys_val} sale(s).\n")
+    return keys_val
+
+
 def main():
     """
     Run all programme functions
     """
     print("This system is complete a sales and advisor review.")
+    print("")
     password_request()
     data = get_sales_data()
 
@@ -114,12 +149,22 @@ def main():
     items_tally = get_new_list(data, 4)
     sales_vals = get_new_list(data, 5)
     values_tally = [int(num) for num in sales_vals]
-    
+
     total_sales = count_total_sales(items_tally)
     adv_list = create_unique_value_list(advisor_tally)
     item_list = create_unique_value_list(items_tally)
     count_sales_value(values_tally, total_sales)
+    print_list(adv_list, item_list)
+    print("Here is a list of the item sales.\n")
+    item_sale_count = create_dict_count(items_tally)
+    # create a function to update the spreadsheet with the figures
+    #display some more info (item with highest sales, item with higest value)
+    higest_item_sales = find_max_key_val(item_sale_count)
 
+    #print("Here is the total sales for the advisors.\n")
+    #adv_sale_count = create_dict_count(advisor_tally)
+    # reuse code to update spreadsheet.
+    # display add info (adv with higest sales, adv's best item, adv with highest value)
 
 
 print('Welcome to the Promotional Sales Review System!\n')
