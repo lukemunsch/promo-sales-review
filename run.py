270e78c9fa1807810b26caad9616d9948037a3c8
lukemunsch <lpmunsch@gmail.com>
2021-10-08 14:22:25 +0000

    rebuild code to complete simplification of defining list

--- run.py
+++ run.py
@@ -46,7 +46,65 @@ def validate_password(value):
         return True
 
 
+def get_sales_data():
+    print("Retrieving all the sales information...")
+    data = SHEET.worksheet('sales')
+    print("Compiling lists...\n")
+    return data
+
+
+def get_new_list(data, column):
+    """
+    This will build a new list for the required sections:
+    - advisor list
+    - items list
+    - values list
+    """
+    a_list = data.col_values(column)
+    del a_list[0]
+    return a_list
+
+
+def count_total_sales(value):
+    """
+    This will count the total number of sales
+    based on one of the lists
+    """
+    count = len(value)
+    print(f"We have found a total of {count} sale(s).\n")
+    return count
+
+
+def count_sales_value(value):
+    """
+    This will calculate the total value of the sales made so far
+    as well as the average sales value.
+    """
     
+
+
+def create_unique_value_list(value):
+    """
+    The function to find only unique values in a list;
+    needed for items and advisors
+    """
+    my_set = set(value)
+    new_list = list(my_set)
+    new_list.sort()
+    return new_list
+
+
+def main():
+    # password_request()
+    data = get_sales_data()
+
+    advisor_tally = get_new_list(data, 3)
+    items_tally = get_new_list(data, 4)
+    values_tally = get_new_list(data, 5)
+    
+    total_sales = count_total_sales(items_tally)
+    adv_list = create_unique_value_list(advisor_tally)
+    item_list = create_unique_value_list(items_tally)
 
 print('Welcome to the Promotional Sales Review System!\n')
 main()
